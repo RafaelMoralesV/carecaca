6c5a76d2f06b3d8c29d30ffcbb3b5a15d4077906
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-05-06 15:05:05 -0400

    Clap as Dependency

--- circus/src/main.rs
+++ circus/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'H', long, default_value = "localhost")]
+    host: String,
+
+    #[arg(short = 'P', long, default_value_t = 3001, value_parser = clap::value_parser!(u16).range(1..))]
+    port: u16,
+}
+
 fn main() {
+    let args = Cli::parse();
+
     println!("Hello from Circus!");
+    println!("Args: {args:?}");
 }
