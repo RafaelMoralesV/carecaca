129444e24a3c5034d2484380aabdf820d0287dbd
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-06-18 18:58:49 -0400

    Basic connection handling

--- clown/src/main.rs
+++ clown/src/main.rs
@@ -1,3 +1,21 @@
+use std::{
+    io::{BufRead, BufReader},
+    net::TcpStream,
+};
+
 fn main() {
-    println!("Hello as a Clown!");
+    let addr = "localhost:3001";
+
+    if let Ok(stream) = TcpStream::connect(addr) {
+        let reader = BufReader::new(stream.try_clone().unwrap());
+
+        for line in reader.lines() {
+            println!("{}", line.unwrap());
+            break;
+        }
+
+        stream.shutdown(std::net::Shutdown::Both).unwrap();
+    } else {
+        println!("Couldn't connect to server...");
+    }
 }
