0d577b0fb3b0d53d1bde0c5e42dcbbb07bbdd41b
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2025-03-12 02:38:29 +0000

    Rendering first few sprites

--- clown/src/main.rs
+++ clown/src/main.rs
@@ -5,9 +5,31 @@ use std::{
 
 use bevy::prelude::*;
 
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("balatro-card-sprites.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2 { x: 71, y: 95 }, 13, 4, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Sprite::from_atlas_image(
+            texture,
+            TextureAtlas {
+                layout: texture_atlas_layout,
+                index: 12,
+            },
+        ),
+        Transform::from_scale(Vec3::splat(2.0)),
+    ));
+}
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
         .insert_resource(ClearColor(Color::BLACK))
         .run();
 
